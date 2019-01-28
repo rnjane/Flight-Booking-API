ca81b81e7461f583ec7e114363b030eccf8d1e50
Robert Njane <robert.njane@andela.com>
2019-01-29 00:09:21 +0300

    ft(flights): flight reminder - Send a reminder email 24 hours prior to a flight [Finishes #162827956]

--- src/bookingproject/settings.py
+++ src/bookingproject/settings.py
@@ -139,3 +139,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #paypal settings
 PAYPAL_RECEIVER_EMAIL = os.environ.get('PAYPAL_RECEIVER_EMAIL')
 PAYPAL_TEST = True
+
+# celery configs
+BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Nairobi'
