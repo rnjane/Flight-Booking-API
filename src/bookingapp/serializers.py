92b51831b0b07bf2ed9b146d8bb159ad1f3570a8
Robert Njane <robert.njane@andela.com>
2019-01-29 10:34:11 +0300

    ch(user auth): validation - Add regex for email and password validation

--- src/bookingapp/serializers.py
+++ src/bookingapp/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from . import models
+from rest_framework.exceptions import ValidationError
+import re
 
 User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
@@ -8,10 +10,19 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email', 'password']
         extra_kwargs = {"password": {"write_only": True}}
+            
 
     def create(self, validated_data):
-        user = User(username=validated_data['username'], email=validated_data['email'])
-        user.set_password(validated_data['password'])
+        email = validated_data['email']
+        username = validated_data['username']
+        password = validated_data['password']
+
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            raise ValidationError("Enter a valid email address.")
+        if not re.match(r'[A-Za-z0-9@#$%^&+=]{8,}', password):
+            raise ValidationError("Enter a valid password. Password should be at least 8 characters long.")
+        user = User(username=username, email=email)
+        user.set_password(password)
         user.save()
         return validated_data
 
